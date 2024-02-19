c2b703cb310b2d58355caa2a146b6161806af260
david <david.evenari@post.runi.ac.il>
2024-02-19 09:13:09 +0200

    try

--- StringOps.java
+++ StringOps.java
@@ -22,18 +22,82 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+    String test = args[0];
+    System.err.println(capVowelsLowRest(test));   
+    String test2 = args[1];
+    System.err.println(camelCase(test2));  
     }
-
+      
+   
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
-    }
+        String ans = "";
+        for(int i = 0 ; i < string.length() ; i++ ){
+            char ch =string.charAt(i);
+            if (ch<96 && 64<ch){
+                ch +=32;
+                ans +=ch;
 
-    public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+            }else{
+                ans +=ch;
+            }
+
+        }
+        String answer ="";
+        for(int i = 0 ; i < ans.length(); i++ ){
+            char ch =ans.charAt(i);
+            if (ch==97 || ch==101 || ch==105 || ch==111 || ch==117){
+               ch = (char) (ch-32);
+               answer += ch;
+            }else{
+                answer += ch;
+            }
+
+
+        }
+        return answer;
     }
+    
+    public static String camelCase (String string) {
+        String ans = "";
+        for(int i = 0 ; i < string.length() ; i++ ){
+            char ch =string.charAt(i);
+            if (ch<96 && 64<ch){
+                ch +=32;
+                ans +=ch;
+
+            }else{
+                ans +=ch;
+            }
+
+        }
+        String answer ="";
+        int i = 0;
+        while (i< ans.length()) {
+            char ch =ans.charAt(i);
+            
+        if (ch==32 && i< (int)(ans.length()-1)&& ans.charAt(i+1)!=32){
+            answer = answer +(char)(ans.charAt(i+1)-32);
+            i++;   
+        }else{
+        answer = answer + ch; }
+        i++;  
+            
+        }
+      
+ 
+    String answerfin ="";
+    for(int j = 0 ; j < answer.length() ; j++ ){
+        char ch = answer.charAt(j);
+        if (ch!=32 ){
+            answerfin +=ch;
+        }
+
+    }
+    char firstChar =(char)(answerfin.charAt(0)+32);
+     String answerfin1= firstChar + answerfin.substring(1);
+    return answerfin1;
+}
+
 
     public static int[] allIndexOf (String string, char chr) {
         // Write your code here:
